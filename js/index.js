4153638c02758d57937cacffb6e2247ac023cc96
Fazila Ahmadi <ahmadi.fa2023@gmail.com>
2026-09-17 10:54:49 +0430

    Add My Booking page with booking management features and responsive design

--- js/index.js
+++ js/index.js
@@ -1,48 +1,18 @@
-// location
-const locationBtn = document.getElementById("locationBtn");
-const locationCard = document.getElementById("locationCard");
-const locationInput = document.getElementById("locationInput");
-// Locatin open and close
-locationBtn .addEventListener("click", function(){
-    locationCard.classList.toggle("active");
-});
-// select countries
-
-const countries = document.querySelectorAll(".country");
-countries.forEach(function(country){
-    country.addEventListener("click",function(){
-        const countryName = country.querySelector("span").textContent;
-        locationInput.value = countryName;
-        locationCard.classList.remove("active");
-    });
-});
-
-// guests
-
-const guestBtn = document.getElementById("guestBtn");
-const guestCard = document.getElementById("guestCard");
-// guests open and close
-guestBtn.addEventListener("click",function(){
-    guestCard.classList.toggle("active");
-})
-
-// select guests and rooms
-const counter = document.querySelectorAll(".counter");
-const minus = document.querySelectorAll(".minus");
-const plus = document.querySelectorAll(".plus");
-
-minus.forEach((button,index)=>{
-    button.addEventListener("click",()=>{
-        let number = parseInt(counter[index].textContent);
-        if (number > 1){
-            counter[index].textContent = number -1
-        }
-    });
-});
-
-plus.forEach((button,index)=>{
-    button.addEventListener("click",()=>{
-        let number = parseInt(counter[index].textContent);
-        counter[index].textContent = number + 1;
-    })
-})
+const countries =[
+    "Kabul, Afghnistan",
+    "Canada",
+    "Dubai",
+    "New-York",
+    "Singapore",
+    "London",
+    "Paris",
+    "Marrakech",
+    "Hong kong",
+    "Torkyo Japan",
+    "Venice"
+];
+const destinations = document.getElementById("destinations");
+const checkIn = document.getElementById("check-in");
+const checkOut = document.getElementById("check-out");
+const guests = document.getElementById("guests");
+const search = document.getElementById("search");
