2dfb312f613cac8e4364ed4e4b9c0fd301f8ccb2
Fazila Ahmadi <ahmadi.fa2023@gmail.com>
2026-09-18 14:13:44 +0430

    Refactor search components across multiple HTML and CSS files; enhance search functionality with dropdowns for destinations and guest selection, and improve layout consistency.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
-const countries =[
-    "Kabul, Afghnistan",
-    "Canada",
-    "Dubai",
-    "New-York",
-    "Singapore",
-    "London",
-    "Paris",
-    "Marrakech",
-    "Hong kong",
-    "Torkyo Japan",
-    "Venice"
-];
-const destinations = document.getElementById("destinations");
-const checkIn = document.getElementById("check-in");
-const checkOut = document.getElementById("check-out");
-const guests = document.getElementById("guests");
-const search = document.getElementById("search");
+
