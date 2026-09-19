f07b9257447c16ecde3e73b169d896ea2461523b
Fazila Ahmadi <ahmadi.fa2023@gmail.com>
2026-09-19 16:17:38 +0430

    Enhance navigation and localization support across multiple pages
    - Updated navigation links in index.html, my-booking.html, payment.html, room.html, services.html, and search-result.html to include data-i18n attributes for localization.
    - Modified login button styles and added data attributes for modal triggering in various HTML files.
    - Improved consistency in language toggle button styling and functionality.
    - Fixed minor typos in footer information across several pages.
    - Added JavaScript functionality for dropdown selection in index.js.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
+const destinationItem = document.getElementById("destinationItem");
+const destinationValue = document.getElementById("destinationValue");
+const destinationList = document.getElementById("destinationList");
+
+const checkinItem = document.getElementById("checkinItem");
+const checkinValue = document.getElementById("checkinValue");
+const checkinInput = document.getElementById("checkinInput");
+
+const checkoutItem = document.getElementById("checkoutItem");
+const checkoutValue = document.getElementById("checkoutValue");
+const checkoutInput = document.getElementById("checkoutInput");
+
+const guestsItem = document.getElementById("guestsItem");
+const guestsValue = document.getElementById("guestsValue");
+const guestsPanel = document.getElementById("guestsPanel");
+const guestDone = document.getElementById("guestDone");
+
+const search = document.getElementById("search");
+
+let guests = {adults: 2, children: 0, rooms: 1};
+
+// open and colse dropdown
+destinationItem.addEventListener("click",(e)=>{
+e.stopPropagation();
+closeAllDropdowns();
+destinationList.classList.toggle("show");
+});
+
+// seclect
+destinationList.querySelectorAll("li").forEach(li =>{
+    li.addEventListener("click",() =>{
+        
+    })
+})
 
